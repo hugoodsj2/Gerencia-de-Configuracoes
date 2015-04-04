227b73ad76585c0157416890462c51f79c3014fe
Hugo Oliveira De São José <hugooliveira48@yahoo.com.br>
2015-04-04 12:26:08 -0300

    Inclusão da tela de cadastro

---
+++ CadastroDeProdutos/Produto/Codigo/scripts/index.js
@@ -0,0 +1,7 @@
+var imported = document.createElement('script');
+imported.src = 'scripts/utils.js';
+document.head.appendChild(imported);
+
+function NovoProduto(){
+		location.href = "cadastro.html";
+}
