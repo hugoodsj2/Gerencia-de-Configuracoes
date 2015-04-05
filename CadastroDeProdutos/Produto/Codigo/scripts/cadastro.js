b7c3cf87e238663fce992a6ca25c750f82d3fe18
Hugo Oliveira De São José <hugooliveira48@yahoo.com.br>
2015-04-05 15:35:07 -0300

    Alterações cadastro e style.css

--- CadastroDeProdutos/Produto/Codigo/scripts/cadastro.js
+++ CadastroDeProdutos/Produto/Codigo/scripts/cadastro.js
@@ -1,1 +1,11 @@
-//Scripts cadastro do produto
+var imported = document.createElement('script');
+imported.src = 'scripts/utils.js';
+document.head.appendChild(imported);
+
+function Voltar(){
+		location.href = "index.html";
+}
+
+function Salvar(){
+		alert("Salvo com sucesso!");
+}
