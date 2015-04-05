0d9e862add4c12afdf8c3335d595dd02978b3c9f
SaraLOliveira <saralima.oliveira@yahoo.com.br>
2015-04-05 16:05:44 -0300

    Inclusao do menu em todas as paginas

--- CadastroDeProdutos/Produto/Codigo/scripts/utils.js
+++ CadastroDeProdutos/Produto/Codigo/scripts/utils.js
@@ -1,3 +1,10 @@
 function value(idElemento) {
 	return document.getElementById(idElemento).value;
 }
+
+function Sair() {
+    var r = confirm("Deseja sair?");
+    if (r) {
+        location.href = "login.html";
+    }
+}
