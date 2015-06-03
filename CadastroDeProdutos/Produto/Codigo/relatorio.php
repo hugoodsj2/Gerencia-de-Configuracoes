6590355361e398bda2d1158e77c353dc6f2244a6
BarbaraAbreu <BarbaraAbreu@users.noreply.github.com>
2015-06-02 21:15:21 -0300

    Colocando ordenação no relatório

--- CadastroDeProdutos/Produto/Codigo/relatorio.php
+++ CadastroDeProdutos/Produto/Codigo/relatorio.php
@@ -1,22 +1,42 @@
 <?php
     $root = getcwd();    
-    include ''.$root.'/include/header.php';
-?>
-	<div id="relConteudo">
+    include ''.$root.'/include/header.php';	
+	function selFiltro( $orderByColumn, $orderByDirection ) {
+		$sql = 'SELECT * FROM produtos ORDER BY '.$orderByColumn.' '.$orderByDirection.';';
+				
+        return $sql;
+        
+    }
+		$conn = mysqli_connect("localhost", "root", "", "tecweb");
+		//$sql = "SELECT CODIGO, DESCRICAO, PRECO, QUANTIDADE FROM produtos";
+		
+		if(isset($_GET['orderByColumn']) && isset($_GET['orderByDirection'])){
+			$sql = selFiltro($_GET['orderByColumn'], $_GET['orderByDirection']);
+			
+		}else{
+			$sql = 'SELECT * FROM produtos;';
+		}			
+		$result = mysqli_query($conn, $sql);
+		// Listar os produtos
+		if (mysqli_num_rows($result) > 0) { // Verifica se existem registros
+ 		// Percorre dados de retorno da consulta SQL	
+ 	?>
+	<div id="relConteudo">		
+		<form action="" method="post">
 		<table id="tabela" cellspacing="0" cellpadding="2px">
 			<thead>
 				<tr>
 					<th align="center">
-						Código
+						<a href="relatorio.php?orderByColumn=codigo&orderByDirection=ASC">Código</a>
 					</th>
 					<th align="center" class="w500"> 
-						Descrição
+						<a href="relatorio.php?orderByColumn=descricao&orderByDirection=ASC">Descrição</a>
 					</th>
 					<th align="center" class="w200">
-						Preço
+						<a href="relatorio.php?orderByColumn=preco&orderByDirection=ASC">Preço</a>
 					</th>
 					<th align="center" class="w100">
-						Quantidade
+						<a href="relatorio.php?orderByColumn=quantidade&orderByDirection=ASC">Quantidade</a>
 					</th>
 				</tr>
 				<tr>
@@ -27,15 +47,7 @@
 				</tr>				
 			</thead>
 			<tbody>
-			<?php
-				$conn = mysqli_connect("localhost", "root", "", "tecweb");
-				$sql = "SELECT CODIGO, DESCRICAO, PRECO, QUANTIDADE FROM produtos";
-				$result = mysqli_query($conn, $sql);
-				// Listar os produtos
-				if (mysqli_num_rows($result) > 0) { // Verifica se existem registros
- 				// Percorre dados de retorno da consulta SQL
- 				 while($row = mysqli_fetch_assoc($result)) {
- 			?>
+				<?php while($row = mysqli_fetch_assoc($result)) {?>
 				<tr>
 					<td align="center">
 						<?php echo " " . $row["CODIGO"] . " ";?>
@@ -50,13 +62,13 @@
 						<?php echo " " . $row["QUANTIDADE"] . "<br>";?>
 					</td>
 			<?php
-
 				 }
-				}
+				}				
 			?>
 				</tr>
 			</tbody>
 		</table>
+		</form>
 	</div>
 	<br>
 		<input type="button" id="btrExibe" class="myButton" onclick="Relatorio()" value="Exibir Tudo">
